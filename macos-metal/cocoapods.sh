2a85771cb931a36a275215e5521428a5cca8f480
Walter Teng <16046667+davzoku@users.noreply.github.com>
2022-08-08 11:33:39 +0800

    add cocoapods apple silicon

---
+++ macos-metal/cocoapods.sh
@@ -0,0 +1,7 @@
+# https://stackoverflow.com/questions/64901180/how-to-run-cocoapods-on-apple-silicon-m1
+
+# Uninstall the local cocoapods gem
+sudo gem uninstall cocoapods
+
+# Reinstall cocoapods via Homebrew
+brew install cocoapods
